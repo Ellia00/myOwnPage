710c60cbed6c78d99dbca6f326079283679aaf33
Ellia00 <elliotborgkvist@gmail.com>
2024-12-16 16:58:37 +0100

    Link opens in new tab + safety

--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -8,7 +8,7 @@ function Education() {
                 <div>
                     <h1>#Education</h1>
                     <p>
-                        I spent two years studying web development at Glimåkra Folkhögskola. The course was structured into twelve modules, each focusing on different aspects of web development. At the end of each module, we completed a case project that allowed us to apply the techniques we had learned.
+                        I spent two years studying web development at <a href="https://www.glimnet.se" target="_blank" rel="noopener noreferrer">Glimåkra Folkhögskola</a>. The course was structured into twelve modules, each focusing on different aspects of web development. At the end of each module, we completed a case project that allowed us to apply the techniques we had learned.
                     </p>
                 </div>
                 <div className="cases">
