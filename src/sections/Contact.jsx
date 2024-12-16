1def48bca5142841672963b36bbd2e16092a1a5b
Ellia00 <elliotborgkvist@gmail.com>
2024-12-16 17:36:04 +0100

    changed picture and classname

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -18,7 +18,7 @@ function Contact() {
   return (
     <section id="contact">
       <div className="image">
-        <img src="/_MG_3993.jpg" alt="Picture of me" />
+        <img src="/315180233_670966547952560_1368967394108862490_n.jpg" alt="Picture of me" />
       </div>
       <div className="text">
         <h1>#Contact</h1>
@@ -28,12 +28,36 @@ function Contact() {
         </p>
 
         <div className="contact-info">
-          <p>#Contact Information</p>
           <ul>
-            <li>
+            <li className="button-parent">
               elliotborgkvist@gmail.com
               <button onClick={copyToClipboard} className="copy-button">
-                Copy
+                <svg
+                  xsvgmlns="http://www.w3.org/2000/svg"
+                  class="ionicon"
+                  viewBox="0 0 512 512"
+                >
+                  <rect
+                    x="128"
+                    y="128"
+                    width="336"
+                    height="336"
+                    rx="57"
+                    ry="57"
+                    fill="none"
+                    stroke="black"
+                    stroke-linejoin="round"
+                    stroke-width="32"
+                  />
+                  <path
+                    d="M383.5 128l.5-24a56.16 56.16 0 00-56-56H112a64.19 64.19 0 00-64 64v216a56.16 56.16 0 0056 56h24"
+                    fill="none"
+                    stroke="black"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="32"
+                  />
+                </svg>
               </button>
               {copySuccess && (
                 <span className="copy-message">{copySuccess}</span>
