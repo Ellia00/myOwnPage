a77a20b44cd9dafb6ceb9cf4c7313e1aeee99621
Ellia00 <elliotborgkvist@gmail.com>
2025-01-29 15:00:24 +0100

    update personal introduction for clarity and conciseness

--- src/sections/Me.jsx
+++ src/sections/Me.jsx
@@ -7,7 +7,7 @@ function Me() {
             <div className="text">
                 <h1>#Me</h1>
                 <p>
-                    Hello there! My name is Elliot. I'm 24 years old and recently finished my web development education. I'm a passionate developer with a curiosity for solving problems and creating beautiful, functional designs. When I’m not coding, you’ll probably find me exploring new tech, gaming, or diving into a good book. I love learning and am always seeking new ways to grow both personally and professionally.
+                    Hello there! My name is Elliot. I'm 24 years old and recently finished my web development education. I'm a passionate developer with a curiosity for solving problems and creating beautiful, functional designs. When I’m not coding, im either gaming, reading or exploring new tech. I love learning and I am always seeking new ways to grow both personally and professionally.
                 </p>
             </div>
         </section>
